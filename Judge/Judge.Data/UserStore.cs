13fa32084522f2825e5a364be4353245b2ee227f
backs <rogatnev.sergey@gmail.com>
2016-02-03 16:23:42 +0600

    - missing files

---
+++ Judge/Judge.Data/UserStore.cs
@@ -0,0 +1,67 @@
+﻿using System.Data.Entity;
+using System.Threading.Tasks;
+using Judge.Model.Entities;
+using Microsoft.AspNet.Identity;
+
+namespace Judge.Data
+{
+    internal sealed class UserStore : IUserPasswordStore<User, long>
+    {
+        private readonly DataContext _context;
+        private readonly DbSet<User> _dbSet;
+
+        public UserStore(string connectionString)
+        {
+            _context = new DataContext(connectionString);
+            _dbSet = _context.Set<User>();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public Task CreateAsync(User user)
+        {
+            _dbSet.Add(user);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task UpdateAsync(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            return _context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(User user)
+        {
+            _dbSet.Remove(user);
+            return _context.SaveChangesAsync();
+        }
+
+        public Task<User> FindByIdAsync(long userId)
+        {
+            return _dbSet.SingleOrDefaultAsync(o => o.Id == userId);
+        }
+
+        public Task<User> FindByNameAsync(string userName)
+        {
+            return _dbSet.SingleOrDefaultAsync(o => o.UserName == userName);
+        }
+
+        public Task SetPasswordHashAsync(User user, string passwordHash)
+        {
+            return Task.FromResult(user.PasswordHash = passwordHash);
+        }
+
+        public Task<string> GetPasswordHashAsync(User user)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(User user)
+        {
+            return Task.FromResult(!string.IsNullOrWhiteSpace(user.PasswordHash));
+        }
+    }
+}
---
+++ Judge/Judge.Model/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity;
+
+namespace Judge.Model.Entities
+{
+    public class User : IUser<long>
+    {
+        public long Id { get; private set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
