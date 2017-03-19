3d7b24bce668ee733267b00e35686f2bafce6a64
Rogatnev Sergey <rogatnev.sergey@gmail.com>
2017-03-19 13:19:10 +0700

    - remove unused references in JudgeService assembly

---
+++ Judge/Judge.Data/AssemblyReferencer.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity.SqlServer;
+
+namespace Judge.Data
+{
+    public static class AssemblyReferencer
+    {
+        public static string GetReference()
+        {
+            return typeof(SqlProviderServices).ToString();
+        }
+    }
+}
