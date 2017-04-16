479d6438ecdd931bc4d0382d4cd5cd36289ce446
Rogatnev Sergey <rogatnev.sergey@gmail.com>
2017-04-16 21:50:32 +0700

    - contests database schema

---
+++ Judge/Judge.Database/dbo/Tables/ContestTasks.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[ContestTasks]
+(
+    [ContestId] INT NOT NULL,
+    [TaskId] BIGINT NOT NULL,
+    [TaskName] nvarchar(5) NOT NULL,
+    CONSTRAINT [PK_ContestTasks] PRIMARY KEY CLUSTERED ([ContestId], [TaskId]),
+    CONSTRAINT [FK_ContestTasks_Contests] FOREIGN KEY ([ContestId]) REFERENCES [dbo].[Contests] ([Id]),
+    CONSTRAINT [FK_ContestTasks_Tasks] FOREIGN KEY ([TaskId]) REFERENCES [dbo].[Tasks] ([Id])
+)
+GO
+CREATE UNIQUE NONCLUSTERED INDEX [UIX_ContestTasks_ContestId_TaskName] 
+    ON [dbo].[ContestTasks] (ContestId, TaskName) WITH (FILLFACTOR = 80)
---
+++ Judge/Judge.Database/dbo/Tables/Contests.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Contests]
+(
+    [Id] INT NOT NULL IDENTITY(1,1), 
+    [Name] NVARCHAR(128) NOT NULL, 
+    [StartTime] DATETIME NOT NULL, 
+    [FinishTime] DATETIME NOT NULL, 
+    [FreezeTime] DATETIME NULL,
+    CONSTRAINT [PK_Contests] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [CK_Contests_FreezeTime] CHECK ([FreezeTime] IS NULL OR ([FreezeTime] > [StartTime] AND [FreezeTime] < [FinishTime]))
+)
