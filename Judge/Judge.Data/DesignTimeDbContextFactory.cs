b810e0ac47dd42ef5002323b5f52b6020507a06b
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2023-11-26 12:14:05 +0700

    remove Configurations.targets

---
+++ Judge/Judge.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace Judge.Data
+{
+    internal sealed class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            return new DataContext("Data Source=.;Initial Catalog=Judge;Integrated Security=True");
+        }
+    }
+}
