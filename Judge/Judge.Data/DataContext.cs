ed3c6b6a4327b0cbfbf823bba9fc87f49fbba1d5
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2023-11-26 11:39:26 +0700

    - move ef mappings

--- Judge/Judge.Data/DataContext.cs
+++ Judge/Judge.Data/DataContext.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
-using Judge.Data.Mappings;
+using Judge.Model.CheckSolution;
+using Judge.Model.Contests;
+using Judge.Model.Entities;
 using Judge.Model.SubmitSolution;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,30 +9,109 @@ namespace Judge.Data
 {
     internal sealed class DataContext : DbContext
     {
-        private readonly string _connectionString;
+        private readonly string connectionString;
 
         public DataContext(string connectionString)
         {
-            _connectionString = connectionString;
+            this.connectionString = connectionString;
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(_connectionString);
+            optionsBuilder.UseSqlServer(this.connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.ApplyConfiguration(new LanguageMapping());
-            modelBuilder.ApplyConfiguration(new UserMapping());
-            modelBuilder.ApplyConfiguration(new SubmitBaseMapping());
-            modelBuilder.ApplyConfiguration(new CheckQueueMapping());
-            modelBuilder.ApplyConfiguration(new SubmitResultMapping());
-            modelBuilder.ApplyConfiguration(new TaskMapping());
-            modelBuilder.ApplyConfiguration(new ContestMapping());
-            modelBuilder.ApplyConfiguration(new ContestTaskMapping());
-            modelBuilder.ApplyConfiguration(new UserRoleMapping());
+            modelBuilder.Entity<Language>(builder =>
+            {
+                builder.HasKey(o => o.Id);
+                builder.ToTable("Languages");
+            });
+
+            modelBuilder.Entity<User>(builder =>
+            {
+                builder.HasKey(o => o.Id);
+                builder.HasMany(o => o.UserRoles)
+                    .WithOne()
+                    .HasForeignKey(o => o.UserId);
+
+                builder.ToTable("Users");
+            });
+
+            modelBuilder.Entity<SubmitBase>(builder =>
+            {
+                builder.HasKey(o => o.Id);
+                builder.Property(o => o.Id).UseIdentityColumn();
+                builder.Property(o => o.SubmitDateUtc).ValueGeneratedOnAdd();
+                builder.Property<byte>("SubmitType");
+
+                builder.HasDiscriminator<byte>(@"SubmitType").HasValue<ProblemSubmit>(1)
+                    .HasValue<ContestTaskSubmit>(2);
+
+                builder.HasMany(o => o.Results)
+                    .WithOne(o => o.Submit)
+                    .HasForeignKey("SubmitId");
+
+                builder.ToTable("Submits", "dbo");
+            });
+
+            modelBuilder.Entity<CheckQueue>(builder =>
+            {
+                builder.HasKey(o => o.SubmitResultId);
+                builder.Property(o => o.CreationDateUtc).ValueGeneratedOnAdd();
+
+                builder.ToTable("CheckQueue", "dbo");
+            });
+
+            modelBuilder.Entity<SubmitResult>(builder =>
+            {
+                builder.HasKey(o => o.Id);
+                builder.Property(o => o.Id).UseIdentityColumn();
+
+                builder.HasOne(o => o.CheckQueue).WithOne();
+
+                builder.ToTable("SubmitResults", "dbo");
+            });
+
+            modelBuilder.Entity<Task>(builder =>
+            {
+                builder.HasKey(o => o.Id);
+                builder.Property(o => o.Id).UseIdentityColumn();
+                builder.Property(o => o.CreationDateUtc).ValueGeneratedOnAdd();
+
+                builder.ToTable("Tasks");
+            });
+
+            modelBuilder.Entity<Contest>(builder =>
+            {
+                builder.HasKey(o => o.Id);
+                builder.Property(o => o.Id).UseIdentityColumn();
+                builder.ToTable("Contests");
+            });
+
+            modelBuilder.Entity<ContestTask>(builder =>
+            {
+                builder.HasKey(o => new {o.ContestId, o.TaskName});
+
+                builder.HasOne(o => o.Task)
+                    .WithMany()
+                    .HasForeignKey(o => o.TaskId);
+
+                builder.HasOne(o => o.Contest)
+                    .WithMany()
+                    .HasForeignKey(o => o.ContestId);
+
+                builder.ToTable("ContestTasks");
+            });
+
+            modelBuilder.Entity<UserRole>(builder =>
+            {
+                builder.HasKey(o => o.Id);
+
+                builder.ToTable("UserRoles");
+            });
         }
 
         public CheckQueue DequeueSubmitCheck()
