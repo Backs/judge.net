23fafecdd4406587ee9ee3f6d3625d6cce2a1404
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2023-12-24 14:03:18 +0700

    - get problem method

---
+++ Judge/Judge.Web.Client/Problems/Problem.cs
@@ -0,0 +1,10 @@
+﻿namespace Judge.Web.Client.Problems;
+
+public class Problem
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = null!;
+    public long MemoryLimitBytes { get; set; }
+    public long TimeLimitMilliseconds { get; set; }
+    public string Statement { get; set; } = null!;
+}
