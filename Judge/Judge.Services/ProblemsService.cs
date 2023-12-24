ad0b7209c0c1265d9a7fd1a492563551982c65c1
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2023-12-24 13:15:43 +0700

    - get problems list

--- Judge/Judge.Services/ProblemsService.cs
+++ Judge/Judge.Services/ProblemsService.cs
@@ -7,48 +7,45 @@ using Judge.Model.CheckSolution;
 using Judge.Model.SubmitSolution;
 using Judge.Web.Client.Problems;
 
-namespace Judge.Services
+namespace Judge.Services;
+
+internal sealed class ProblemsService : IProblemsService
 {
-    internal sealed class ProblemsService : IProblemsService
+    private readonly IUnitOfWorkFactory unitOfWorkFactory;
+
+    public ProblemsService(IUnitOfWorkFactory unitOfWorkFactory)
     {
-        private readonly IUnitOfWorkFactory unitOfWorkFactory;
+        this.unitOfWorkFactory = unitOfWorkFactory;
+    }
 
-        public ProblemsService(IUnitOfWorkFactory unitOfWorkFactory)
+    public async Task<ProblemsList> GetProblemsAsync(long? userId, ProblemsQuery query)
+    {
+        await using var unitOfWork = this.unitOfWorkFactory.GetUnitOfWork(false);
+        var tasks = await unitOfWork.TaskRepository.GetTasksAsync(OpenedTasksSpecification.Instance, query.Skip,
+            query.Take);
+
+        var totalCount = await unitOfWork.TaskRepository.CountAsync(OpenedTasksSpecification.Instance);
+
+        var solved = new HashSet<long>();
+
+        if (userId != null)
         {
-            this.unitOfWorkFactory = unitOfWorkFactory;
+            var userSolved = await unitOfWork.SubmitResultRepository.GetSolvedProblemsAsync(
+                new UserSolvedProblemsSpecification(userId.Value, tasks.Select(o => o.Id).ToImmutableHashSet()));
+            solved.UnionWith(userSolved);
         }
 
-        public async Task<ProblemsList> GetProblemsAsync(ProblemsQuery query)
+        var problems = tasks.Select(o => new ProblemInfo
         {
-            await using var unitOfWork = this.unitOfWorkFactory.GetUnitOfWork(false);
-            var tasks = await unitOfWork.TaskRepository.GetTasksAsync(OpenedTasksSpecification.Instance, query.Skip,
-                query.Take);
+            Id = o.Id,
+            Name = o.Name,
+            Solved = solved.Contains(o.Id)
+        }).ToArray();
 
-            var totalCount = await unitOfWork.TaskRepository.CountAsync(OpenedTasksSpecification.Instance);
-
-            var solved = new HashSet<long>();
-
-            var userId = (long?) 1;
-
-            if (userId != null)
-            {
-                var userSolved = await unitOfWork.SubmitResultRepository.GetSolvedProblemsAsync(
-                    new UserSolvedProblemsSpecification(userId.Value, tasks.Select(o => o.Id).ToImmutableHashSet()));
-                solved.UnionWith(userSolved);
-            }
-
-            var problems = tasks.Select(o => new ProblemInfo
-            {
-                Id = o.Id,
-                Name = o.Name,
-                Solved = solved.Contains(o.Id)
-            }).ToArray();
-
-            return new ProblemsList
-            {
-                Items = problems,
-                TotalCount = totalCount
-            };
-        }
+        return new ProblemsList
+        {
+            Items = problems,
+            TotalCount = totalCount
+        };
     }
 }
