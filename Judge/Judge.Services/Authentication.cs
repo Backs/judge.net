79cbd86946aa30e63155d7867e2e9a3dac9c2c55
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2023-12-24 13:49:17 +0700

    - authentication

---
+++ Judge/Judge.Services/Authentication.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Judge.Web.Client.Users;
+
+namespace Judge.Services;
+
+public record Authentication(AuthenticationResult Result, User? User, string[] Roles)
+{
+    public static Authentication UserNotFound() =>
+        new Authentication(AuthenticationResult.UserNotFound, null, Array.Empty<string>());
+
+    public static Authentication Success(User user, string[] roles) =>
+        new Authentication(AuthenticationResult.Success, user, roles);
+
+    public static Authentication PasswordVerificationFailed() =>
+        new Authentication(AuthenticationResult.PasswordVerificationFailed, null, Array.Empty<string>());
+}
