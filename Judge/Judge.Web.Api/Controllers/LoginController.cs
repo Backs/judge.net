5ab6079a670f6d657a38c3d00ecbcfbb5cc07b58
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2024-02-08 22:45:33 +0700

    - small fixes

--- Judge/Judge.Web.Api/Controllers/LoginController.cs
+++ Judge/Judge.Web.Api/Controllers/LoginController.cs
@@ -29,26 +29,25 @@ public class LoginController : ControllerBase
     [HttpPost("token")]
     public async Task<IActionResult> CreateToken([FromBody] Login login)
     {
-        var authentication = await this.securityService.AuthenticateAsync(login);
+        var (authenticationResult, user, roles) = await this.securityService.AuthenticateAsync(login);
 
-        if (authentication.Result == AuthenticationResult.UserNotFound)
+        if (authenticationResult == AuthenticationResult.UserNotFound)
             return this.NotFound();
 
-        if (authentication.Result == AuthenticationResult.PasswordVerificationFailed)
+        if (authenticationResult == AuthenticationResult.PasswordVerificationFailed)
             return this.Unauthorized();
 
-        if (authentication.Result == AuthenticationResult.Success)
+        if (authenticationResult == AuthenticationResult.Success)
         {
             var key = Encoding.ASCII.GetBytes(this.configuration["AppSettings:SecurityKey"]);
-            var user = authentication.User;
             var claims = new List<Claim>(4)
             {
-                new Claim("Id", user!.Id.ToString()),
-                new Claim(JwtRegisteredClaimNames.Sub, user.Login),
-                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+                new("Id", user!.Id.ToString()),
+                new(JwtRegisteredClaimNames.Sub, user.Login),
+                new(JwtRegisteredClaimNames.Email, user.Email)
             };
 
-            claims.AddRange(authentication.Roles.Select(o => new Claim(ClaimTypes.Role, o)));
+            claims.AddRange(roles.Select(o => new Claim(ClaimTypes.Role, o)));
 
             var tokenDescriptor = new SecurityTokenDescriptor
             {
@@ -62,7 +61,7 @@ public class LoginController : ControllerBase
             var tokenHandler = new JwtSecurityTokenHandler();
             var token = tokenHandler.CreateToken(tokenDescriptor);
             var jwtToken = tokenHandler.WriteToken(token);
-            return this.Ok(new TokenResult {Token = jwtToken});
+            return this.Ok(new TokenResult { Token = jwtToken });
         }
 
         return this.BadRequest();
