19b603996b17a35c634b14c6fc346554584ec4f0
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2024-02-09 21:38:53 +0700

    - unhandled exceptions handling

---
+++ Judge/Judge.Web.Api/Middleware/ErrorHandlerMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Judge.Web.Api.Middleware;
+
+public class ErrorHandlerMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception exception)
+        {
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            var problemDetails = CreateProblemDetails(context, exception);
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+
+    private static ProblemDetails CreateProblemDetails(in HttpContext context, in Exception exception)
+    {
+        var statusCode = context.Response.StatusCode;
+        var reasonPhrase = ReasonPhrases.GetReasonPhrase(statusCode);
+
+        var problemDetails = new ProblemDetails
+        {
+            Status = statusCode,
+            Title = reasonPhrase,
+            Detail = exception.ToString(),
+            Extensions =
+            {
+                ["traceId"] = Activity.Current?.Id ?? context.TraceIdentifier,
+                ["data"] = exception.Data
+            }
+        };
+
+
+        return problemDetails;
+    }
+}
