932abe9d26c72ed00db4c3a9dea18d3145b14428
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2024-02-22 22:30:06 +0700

    - data mapping

---
+++ Judge/Judge.Data/Migrations/20240222145304_datetime.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Judge.Data.Migrations
+{
+    public partial class datetime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SubmitDateUtc",
+                schema: "dbo",
+                table: "Submits",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDateUtc",
+                schema: "dbo",
+                table: "CheckQueue",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDateUtc",
+                table: "Tasks",
+                type: "datetime",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.Sql(@"
+CREATE PROCEDURE [dbo].[DequeueSubmitCheck]
+AS
+BEGIN
+
+	;WITH CTE(SubmitResultId, CreationDateUtc)
+	AS
+	(
+		SELECT TOP 1 *
+		FROM dbo.CheckQueue cq WITH(UPDLOCK, READPAST)
+		ORDER BY cq.CreationDateUtc
+	)
+	DELETE CTE
+	OUTPUT
+		DELETED.*
+END");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "SubmitDateUtc",
+                schema: "dbo",
+                table: "Submits",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDateUtc",
+                schema: "dbo",
+                table: "CheckQueue",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreationDateUtc",
+                table: "Tasks",
+                type: "datetime",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime",
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.Sql("DROP PROCEDURE [dbo].[DequeueSubmitCheck]");
+        }
+    }
+}
