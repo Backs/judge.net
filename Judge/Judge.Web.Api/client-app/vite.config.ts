5795436c3dd21d51b955ea9c69fc0022e4ec9b23
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2024-05-11 19:50:19 +0700

    - configuration

--- Judge/Judge.Web.Api/client-app/vite.config.ts
+++ Judge/Judge.Web.Api/client-app/vite.config.ts
@@ -1,10 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl';
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
+    plugins: [
+        react(),
+        basicSsl()
+    ],
+    build: {
+        outDir: 'build',
+    },
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
+    },
+    server: {
+        host: 'localhost',
+        port: 44402,
+        strictPort: true,
+    },
 })
